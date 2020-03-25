bb15e0edf35ffb9725b5c998f89f292b0cf526ac
Aman Gupta <amgupta1@linkedin.com>
2020-03-25 00:57:41 -0700

    Add resource key validation while parsing

--- restli-common/src/test/java/com/linkedin/restli/common/TestComplexResourceKey.java
+++ restli-common/src/test/java/com/linkedin/restli/common/TestComplexResourceKey.java
@@ -22,6 +22,7 @@ import com.linkedin.data.template.DataTemplateUtil;
 import com.linkedin.data.template.RecordTemplate;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestComplexResourceKey
@@ -126,30 +127,59 @@ public class TestComplexResourceKey
     }
   }
 
-  @Test
-  public void testKeySchema()
+  @DataProvider
+  public Object[][] keySchemaValidation() {
+    return new Object[][]
+        {
+            {11, 11, false},
+            {1, 1, true},
+            {11, 1, false},
+            {1, 11, true}
+        };
+  }
+
+  @Test(dataProvider = "keySchemaValidation")
+  public void testKeySchema(int keyValue, int paramValue, boolean validationFailure)
   {
     RecordDataSchema schema = OmniRecord.schema;
     TypeSpec<OmniRecord> keyType = new TypeSpec<OmniRecord>(OmniRecord.class, schema);
-    TypeSpec<OmniRecord> paramsType = new TypeSpec<OmniRecord>(OmniRecord.class, schema);
-    ComplexKeySpec<OmniRecord, OmniRecord> keySpec = new ComplexKeySpec<OmniRecord, OmniRecord>(keyType, paramsType);
-
+    TypeSpec<NullSchemaRecord> paramsType = new TypeSpec<NullSchemaRecord>(NullSchemaRecord.class, null);
+    ComplexKeySpec<OmniRecord, NullSchemaRecord> keySpec =
+        new ComplexKeySpec<OmniRecord, NullSchemaRecord>(keyType, paramsType);
+    DataMap paramsData = new DataMap();
+    paramsData.put("int", paramValue);
     DataMap data = new DataMap();
-    data.put("int", 1);
+    data.put("int", keyValue);
+    data.put("$params", paramsData);
 
-    ComplexResourceKey<RecordTemplate, RecordTemplate> key = ComplexResourceKey.buildFromDataMap(data, keySpec);
-
-    Assert.assertEquals(key.getKey().schema(), schema);
-    Assert.assertEquals(key.getParams().schema(), schema);
+    try
+    {
+      ComplexResourceKey<RecordTemplate, RecordTemplate> key = ComplexResourceKey.buildFromDataMap(data, keySpec);
+      key.validate();
+      Assert.assertEquals(key.getKey().schema(), schema);
+      Assert.assertNull(key.getParams().schema());
+      Assert.assertFalse(validationFailure);
+    }
+    catch (IllegalArgumentException ex)
+    {
+      Assert.assertTrue(validationFailure, "Unexpected validation failure");
+    }
   }
 
   public static class OmniRecord extends RecordTemplate {
     private static RecordDataSchema schema =
-            (RecordDataSchema) DataTemplateUtil.parseSchema("{ \"type\" : \"record\", \"name\" : \"omni\", \"fields\" : [ { \"name\" : \"int\", \"type\" : \"int\" } ] }");
+            (RecordDataSchema) DataTemplateUtil.parseSchema("{ \"type\" : \"record\", \"name\" : \"omni\", \"fields\" : [ { \"name\" : \"int\", \"type\" : \"int\", \"validate\": { \"regex\": { \"regex\": \"[0-9][0-9]\" } } } ] }");
 
     public OmniRecord(DataMap map)
     {
       super(map, schema);
+    }
+  }
+
+  public static class NullSchemaRecord extends RecordTemplate {
+    public NullSchemaRecord(DataMap map)
+    {
+      super(map, null);
     }
   }
 }
